3275325ee7a6dd0d1c2c3d69093b3f670e8e15b7
debanjan <debanjan@pitangent.com>
2024-06-17 13:11:47 +0530

    backend complete

--- backend/controller/appointmentController.js
+++ backend/controller/appointmentController.js
@@ -92,20 +92,34 @@ export const getAllAppointments = catchAsyncErrors(async(req, res, next)=>{
 
 
 export const updateAppointmentStatus = catchAsyncErrors(
-    async (req, res, next) => {
-      const { id } = req.params;
-      let appointment = await Appointment.findById(id);
-      if (!appointment) {
-        return next(new ErrorHandler("Appointment not found!", 404));
-      }
-      appointment = await Appointment.findByIdAndUpdate(id, req.body, {
-        new: true,
-        runValidators: true,
-        useFindAndModify: false,
-      });
-      res.status(200).json({
-        success: true,
-        message: "Appointment Status Updated!",
-      });
+  async (req, res, next) => {
+    const { id } = req.params;
+    let appointment = await Appointment.findById(id);
+    if (!appointment) {
+      return next(new ErrorHandler("Appointment not found!", 404));
     }
-  );
+    appointment = await Appointment.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Appointment Status Updated!",
+      appointment,
+    });
+  }
+);
+
+export const deleteAppointment = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  const appointment = await Appointment.findById(id);
+  if (!appointment) {
+    return next(new ErrorHandler("Appointment Not Found!", 404));
+  }
+  await appointment.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Appointment Deleted!",
+  });
+});
--- backend/router/appointmentRouter.js
+++ backend/router/appointmentRouter.js
@@ -1,12 +1,13 @@
 import express from "express"
-import { getAllAppointments, postAppointment, updateAppointmentStatus } from "../controller/appointmentController.js"
+import { deleteAppointment, getAllAppointments, postAppointment, updateAppointmentStatus } from "../controller/appointmentController.js"
 import { isAdminAuthenticated, isPatientAuthenticated} from "../middlewares/auth.js"
 
 const router = express.Router()
 
 router.post("/post", isPatientAuthenticated, postAppointment)
 router.get("/getall", isAdminAuthenticated, getAllAppointments)
-router.get("/update/:id", isAdminAuthenticated, updateAppointmentStatus)
+router.put("/update/:id", isAdminAuthenticated, updateAppointmentStatus)
+router.delete("/delete/:id", isAdminAuthenticated, deleteAppointment)
 
 
 export default router;
